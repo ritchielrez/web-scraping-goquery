02545c5b6b64d9513538bc55337f50467174d424
RitchielRez <ritchielrez@gmail.com>
2022-04-21 23:48:23 +0600

    fix(main): better comments

--- main.go
+++ main.go
@@ -15,6 +15,11 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error: %v\n", err)
 	}
+
+	// `defer` is really important here to use
+	// so it only closes the response, if
+	// only there isn't any response
+
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
 		log.Fatalf("Status code error: %d %s\n", res.StatusCode, res.Status)
@@ -26,13 +31,15 @@ func main() {
 		log.Fatalf("Error: %v\n", err)
 	}
 
+	// The verbose method
 	// doc.Find(".col-md-9").EachWithBreak(func(i int, s *goquery.Selection) bool {
-	// 	title := strings.Split(s.Find("code").Text(), "\n")[0]
+	// 	version := strings.Split(s.Find("code").Text(), "\n")[0]
 	// 	fmt.Println(title)
 	// 	return false
 	// })
 
-  version := strings.Split(doc.Find("div.col-md-9 code").Text(), "\n")[0]
-  fmt.Println(version)
+	// The quick css selector trick to scrape content
+	version := strings.Split(doc.Find("div.col-md-9 code").Text(), "\n")[0]
+	fmt.Println(version)
 
 }
